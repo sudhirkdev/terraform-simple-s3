82a0b1dc6f18a29be2840b578007fe4636e754e7
sudhirkdev <158827942+sudhirkdev@users.noreply.github.com>
2024-02-25 17:08:20 +0530

    Update main.tf
    Update and working one

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 5.0"
+   }
+  }
+}
+
 resource "aws_s3_bucket" "example" {
-  bucket = "simple-test-bucket-11"
+  bucket = "simple-test-bucket-12"
 
   tags = {
-    Name        = "simple-test-bucket-11"
+    Name        = "simple-test-bucket-12"
     Environment = "Dev"
   }
 }
+
